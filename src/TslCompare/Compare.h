6ed24502d1bc1941eb68f56a7e5629a1a901de4d
Ivan <zakharovivann@gmail.com>
2023-05-08 21:49:08 +0300

    add cmakes

---
+++ src/TslCompare/Compare.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Graph.h"
+#include "GraphAlgorithms.h"
+//#include "../lib/time.h"
+#include "../submodules/cpp_libs/utility/time.h"
+
+namespace s21 {
+
+typedef std::vector<int64_t> TimeResult;
+
+struct CompareResult {
+    TimeResult time;
+    std::vector<double> mean;
+    CompareResult(int N) : mean(N) {}
+};
+
+class Compare {
+    public:
+        static TimeResult Time(Graph &G, int N);
+        static CompareResult Full(Graph &G, int N);
+};
+
+} // namespace s21
